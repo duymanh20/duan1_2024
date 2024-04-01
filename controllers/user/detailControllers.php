b1bcb99ef064f880731311a015ef8c6713debf4e
tung <tungdaph36979@fpt.edu.vn>
2024-04-01 23:39:14 +0700

    thêm chức năng mới

---
+++ controllers/user/detailControllers.php
@@ -0,0 +1,8 @@
+<?php
+
+if(isset($_GET['id'])){
+    $getSanPhamById=getSanPhamById($_GET['id']);
+    $getAllSizeSanPham=getAllSizeSanPham($_GET['id']);
+    increaseView($_GET['id']);
+}
+include_once("views/user/detail.views.php");
